d25830faafbc026e3fd601c521843eb9854f3a05
stefanigita <stefanigermatonna@gmail.com>
2023-06-16 20:03:50 +0700

    update js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-+function validate(){
+function validate(){
     const username = document.getElementById("username").value
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
